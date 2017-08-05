c95b64c33b8d66274752880cd68e3b44da6db93c
Mario Danic <mario@lovelyhq.com>
2017-08-05 15:52:26 +0200

    Retain search
    Signed-off-by: Mario Danic <mario@lovelyhq.com>

--- src/main/java/com/owncloud/android/ui/activity/FileDisplayActivity.java
+++ src/main/java/com/owncloud/android/ui/activity/FileDisplayActivity.java
@@ -698,6 +698,7 @@ public class FileDisplayActivity extends HookActivity
 
         if (fileListFragment != null) {
             fileListFragment.setSearchFragment(false);
+            fileListFragment.setFabEnabled(true);
         }
     }
 
@@ -862,12 +863,14 @@ public class FileDisplayActivity extends HookActivity
             case android.R.id.home: {
                 FileFragment second = getSecondFragment();
                 OCFile currentDir = getCurrentDir();
-                if (isDrawerOpen()) {
+                if (searchView != null && searchView.isFocused()) {
+                    searchView.clearFocus();
+                    break;
+                } else if (isDrawerOpen()) {
                     closeDrawer();
                 } else if ((currentDir != null && currentDir.getParentId() != 0) ||
                         (second != null && second.getFile() != null) || isSearchOpen()) {
                     onBackPressed();
-
                 } else {
                     openDrawer();
                 }
@@ -1142,6 +1145,9 @@ public class FileDisplayActivity extends HookActivity
             searchView.setQuery("", true);
             searchView.onActionViewCollapsed();
             setDrawerIndicatorEnabled(isDrawerIndicatorAvailable());
+            resetSearchView();
+            refreshListOfFilesFragment(true);
+
         } else if (isDrawerOpen) {
             // close drawer first
             super.onBackPressed();
@@ -2465,11 +2471,18 @@ public class FileDisplayActivity extends HookActivity
 
     private void refreshList(boolean ignoreETag) {
         OCFileListFragment listOfFiles = getListOfFilesFragment();
-        if (listOfFiles != null && !listOfFiles.isSearchFragment()) {
+        if (listOfFiles != null && !listOfFiles.isSearchFragment() && (searchView == null ||
+                TextUtils.isEmpty(searchView.getQuery()))) {
             OCFile folder = listOfFiles.getCurrentFile();
             if (folder != null) {
                 startSyncFolderOperation(folder, ignoreETag);
             }
+        } else if (searchView != null && !TextUtils.isEmpty(searchView.getQuery())) {
+            searchView.setQuery(searchView.getQuery(), true);
+            if (listOfFiles != null) {
+                listOfFiles.onQueryTextSubmit(searchView.getQuery().toString());
+            }
+
         }
     }
 
