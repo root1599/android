44fd4d3a528ad913d18ecf40ea96137ba6cf2bf3
AndyScherzinger <info@andy-scherzinger.de>
2018-08-29 14:28:17 +0200

    remove tray/catch due to changed method signature (after rebase)

--- src/gplay/java/com/owncloud/android/utils/PushUtils.java
+++ src/gplay/java/com/owncloud/android/utils/PushUtils.java
@@ -55,7 +55,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.KeyFactory;
@@ -378,19 +377,11 @@ public class PushUtils {
                 PreferenceManager.setKeysMigration(context, true);
             } else {
                 if (oldPrivateKeyFile.exists()) {
-                    try {
-                        FileStorageUtils.moveFile(oldPrivateKeyFile, privateKeyFile);
-                    } catch (IOException e) {
-                        Log.e(TAG, "Failed to move old private key to new location");
-                    }
+                    FileStorageUtils.moveFile(oldPrivateKeyFile, privateKeyFile);
                 }
 
                 if (oldPublicKeyFile.exists()) {
-                    try {
-                        FileStorageUtils.moveFile(oldPublicKeyFile, publicKeyFile);
-                    } catch (IOException e) {
-                        Log.e(TAG, "Failed to move old public key to new location");
-                    }
+                    FileStorageUtils.moveFile(oldPublicKeyFile, publicKeyFile);
                 }
 
                 if (privateKeyFile.exists() && publicKeyFile.exists()) {
